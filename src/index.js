b03c46e74fc9fe30a28918f09e817dd606714540
baoyudai <daibaoyu1122@icloud.com>
2026-01-28 23:38:35 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
-
-
-I've fixed the `index.html` link to Tailwind CSS and refined the code structure. Once you update these files on GitHub, your website's format will reappear instantly!
